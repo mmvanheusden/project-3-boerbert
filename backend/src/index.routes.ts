078535af48a7a2d70aa0420997b81187eaed0b0f
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-09 11:53:47 +0100

    feat: add slots schema for database
    Signed-off-by: Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>

--- backend/src/index.routes.ts
+++ backend/src/index.routes.ts
@@ -1,10 +1,12 @@
 import Elysia from 'elysia';
 import {ActivitiesController} from "./activities";
 import { SlideshowController } from './slideshow';
+import {SlotsController} from "./slots";
 
 
 const routes = new Elysia()
     .use(ActivitiesController)
-    .use(SlideshowController);
+    .use(SlideshowController)
+    .use(SlotsController)
 
 export { routes as AppRoutes };
