0600abc16d18f568c60f4a23a3442488cb46b1ea
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-12-10 18:05:47 +0100

    fix: building frontend by migrating backend from npm to bun and loosening linter config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,19 @@
 import {Elysia} from 'elysia'
-import {node} from '@elysiajs/node'
 import 'dotenv/config';
 import {AppRoutes} from "./index.routes";
 import openapi, {fromTypes} from "@elysiajs/openapi";
 import cors from "@elysiajs/cors";
 
-const app = new Elysia({adapter: node()})
+const app = new Elysia()
     .use(openapi({
         references: fromTypes()
     }))
     .use(cors())
     .use(AppRoutes)
-	.get("/ping", () => "pong") // /ping geeft terug pong
+	.get("/ping", () => "pong") // ping geeft terug pong
     .listen(3000, ({hostname, port}) => {
         console.log(
-            `🦊 Elysia is running at ${hostname}:${port}`
+            `De backend runt op: ${hostname}:${port} !!!!`
         )
     })
 
