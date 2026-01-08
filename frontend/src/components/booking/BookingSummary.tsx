63c6ff8912e14bdb225f861d4eaf65817fd8fc8d
Reageerbuisje <loek.engelbarts@gmail.com>
2026-01-08 11:48:19 +0100

    BookingSummary styling hersteld

--- frontend/src/components/booking/BookingSummary.tsx
+++ frontend/src/components/booking/BookingSummary.tsx
@@ -16,43 +16,44 @@ export function BookingSummary() {
 				</span>
 			</Header>
 
-			<div className="flex-1 overflow-auto text-2xl ">
-				<div className="w-full h-full overflow-auto flex flex-col  items-center bg-white shadow-md rounded-lg">
-					<div className=" min-h-[30vh] max-h-[40vh] overflow-hidden rounded-4xl outline-black outline-3 mt-10 mb-15">
+			<div className="flex-1 overflow-auto text-2xl p-5">
+				<div>
+					<div className="w-full h-[20vh] max-h-[20vh] overflow-hidden rounded-xl">
 						<img
-							className="object-contain size-full "
-							src={`data:image/png;base64, ${context.selectedActivity?.hero}`}
+							className="w-full h-full object-cover object-center"
+							src={`data:image/png;base64, ${selectedActivity?.hero}`}
 							style={{ imageRendering: "pixelated" }}
-							alt={context.selectedActivity?.title[i18n.language as "en" | "de" | "nl"] ?? "activity image"}
+							alt={selectedActivity?.title[i18n.language as "en" | "de" | "nl"] ?? "activity image"}
 						/>
 					</div>
-				<h1 className="text-6xl font-semibold mb-10 text-center">Weet je zeker dat je deze activiteit wil boeken?</h1>
-				<b className="text-center text-5xl mb-5">Boekingsdetails:</b>
+					<h1 className="text-8xl font-semibold mb-15 mt-10 text-center">Weet je zeker dat je deze activiteit wil boeken?</h1>
+					<b className="text-center text-6xl">Boekingsdetails:</b>
 					<div className="text-4xl">
-					<BookingDetails/>
+						<BookingDetails />
 					</div>
 				</div>
 			</div>
 			<div className="flex-row flex w-full items-center justify-between">
 
 				<button
-					className="hover:cursor-pointer rounded-xl py-10 px-10 bg-green-600 hover:bg-green-700 focus:outline-none text-7xl text-white"
+					className="cursor-pointer w-full rounded-xl py-10 px-10 bg-green-600 hover:bg-green-700 focus:outline-none text-7xl text-white"
 					onClick={context.next}>
 					{t("continue_bookingflow")}
 				</button>
 			</div>
-			 <div 
-											className="flex-row flex w-full items-center justify-between">
-											<CancelButton/>
-				                            <div>
-                                <button
-                                    className="hover:cursor-pointer rounded-xl py-3 px-5 bg-orange-400 hover:bg-orange-300 focus:outline-none text-4xl mr-3 text-white"
-                                    onClick={context.prev}>
-                                    {t("Terug naar activiteitweergave")}
-                                </button>
-                            </div>
 
+
+	  <div 
+					className="flex-row flex w-full items-center justify-between">
+					<CancelButton/>
+				<div>
+					<button
+						className="hover:cursor-pointer rounded-xl py-3 px-5 bg-orange-400 hover:bg-orange-300 focus:outline-none text-4xl mr-3 text-white"
+						onClick={context.prev}>
+						{t("Terug naar activiteit bekijken invoeren")}
+					</button>
+				</div>
 			</div>
 		</div>
-	)
+	);
 }
