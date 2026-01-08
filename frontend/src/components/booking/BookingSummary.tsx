98bd3c8df9f828d223b7a1087af6a3ca051f5db1
tahaqamch <taha.qamch@student.hu.nl>
2026-01-08 10:19:53 +0100

    Jewey's merge conflict opgelost (Het was Maarten)

--- frontend/src/components/booking/BookingSummary.tsx
+++ frontend/src/components/booking/BookingSummary.tsx
@@ -15,22 +15,7 @@ export function BookingSummary() {
 					{t("boeking_samenvatting")}
 				</span>
 			</Header>
-<<<<<<< Updated upstream
-			<div className="flex-1 overflow-auto text-2xl p-5">
-				<div>
-					<div className="w-full h-[20vh] max-h-[20vh] overflow-hidden rounded-xl">
-						<img
-							className="w-full h-full object-cover object-center"
-							src={`data:image/png;base64, ${selectedActivity?.hero}`}
-							style={{ imageRendering: "pixelated" }}
-							alt={selectedActivity?.title[i18n.language as "en" | "de" | "nl"] ?? "activity image"}
-						/>
-					</div>
-				</div>
-				<h1 className="text-8xl font-semibold mb-15 mt-10 text-center">Weet je zeker dat je deze activiteit wil boeken?</h1>
-				<b className="text-center text-6xl">Boekingsdetails:</b>
-				<BookingDetails />
-=======
+
 			<div className="flex-1 overflow-auto text-2xl ">
 				<div className="w-full h-full overflow-auto flex flex-col  items-center bg-white shadow-md rounded-lg">
 					<div className=" min-h-[30vh] max-h-[40vh] overflow-hidden rounded-4xl outline-black outline-3 mt-10 mb-15">
@@ -49,7 +34,7 @@ export function BookingSummary() {
 				</div>
 			</div>
 			<div className="flex-row flex w-full items-center justify-between">
->>>>>>> Stashed changes
+
 				<button
 					className="hover:cursor-pointer rounded-xl py-10 px-10 bg-green-600 hover:bg-green-700 focus:outline-none text-7xl text-white"
 					onClick={context.next}>
