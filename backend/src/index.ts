14b531e09b6739471a474a0d6791078ad6366d81
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2026-01-19 13:38:51 +0100

    feat: booking confirmation emails

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,18 @@ import 'dotenv/config';
 import {AppRoutes} from "./index.routes";
 import openapi, {fromTypes} from "@elysiajs/openapi";
 import cors from "@elysiajs/cors";
+import nodemailer from "nodemailer"
+
+/* Turns a "." into a "," (localization) */
+declare global {
+    interface String {
+        dot2comma(): string;
+    }
+}
+String.prototype.dot2comma = function() {
+    return this.replace(".", ",");
+};
+
 
 const app = new Elysia()
     .use(openapi({
@@ -20,3 +32,23 @@ const app = new Elysia()
     })
 
 export type ElysiaApp = typeof app
+
+/* Mailtrap email configuratie */
+export const EmailTransporter = nodemailer.createTransport(
+    {
+        host: 'live.smtp.mailtrap.io',
+        port: 2525,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: 'api',
+            pass: process.env.MAILTRAP_TOKEN!
+        },
+    },
+    {
+        from: {
+            address: process.env.MAILTRAP_SENDER!,
+            name: "Boerencamping De Groene Weide",
+        }
+    }
+)
