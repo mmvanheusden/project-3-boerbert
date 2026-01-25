a5a9a2fb71a606e6d86ab4e655bb1c0a42f99d29
Reageerbuisje <loek.engelbarts@gmail.com>
2026-01-25 20:21:25 +0100

    dood en verderf

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ const app = new Elysia()
         references: fromTypes()
     }))
     .use(cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173"
+        origin: process.env.FRONTEND_URL || ""
     }))
     .use(cron({
         name: "reminder_email",
